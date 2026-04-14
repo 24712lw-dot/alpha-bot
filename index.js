e105864b0294c5d137c84b3089a207e8de6fb104
24712lw-dot <24712lw@gmail.com>
2026-04-14 21:31:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once('ready', () => {
+const commands = [
+  {
+    name: 'ping',
+    description: 'Check bot status'
+  }
+];
+
+client.once('ready', async () => {
   console.log(`Logged in as ${client.user.tag}`);
+
+  try {
+    const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+    await rest.put(
+      Routes.applicationCommands(client.user.id),
+      { body: commands }
+    );
+
+    console.log('Slash command registered!');
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 client.on('interactionCreate', async interaction => {
